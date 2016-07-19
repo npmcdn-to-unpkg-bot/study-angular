28f342972a845df43e5d26de04c49fa28954fb80
Christian Marcell de Oliveira <chroda@chroda.com.br>
2016-07-19 12:40:41 -0300

    aula8 - criando servicos

--- rbranas/listaTelefonica/js/controllers/listaTelefonicaCtrl.js
+++ rbranas/listaTelefonica/js/controllers/listaTelefonicaCtrl.js
@@ -1,30 +1,30 @@
-app.controller("listaTelefonicaCtrl", function($scope, $http){
+app.controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator){
   $scope.app = "Lista Telefonica";
-  $scope.backendPath = "http://localhost/study-angular/rbranas/listaTelefonica/backend/";
   $scope.contatos = [];
   $scope.operadoras = [];
 
   var carregarContatos = function(){
-    $http.get($scope.backendPath + "contatos.json").success(function(data){
+    contatosAPI.getContatos().success(function(data){
       $scope.contatos = data;
     }).error(function(data, status){
       $scope.message = "Aconteceu um problema: " + data;
     });
   }
-  carregarContatos();
+
   var carregarOperadoras = function(){
-    $http.get($scope.backendPath + "operadoras.json").success(function(data){
+    operadorasAPI.getOperadoras().success(function(data){
       $scope.operadoras = data;
     });
   }
-  carregarOperadoras();
 
   $scope.adicionarContato = function(contato){
-    $scope.contatos.push(angular.copy(contato));
-    $http.post($scope.backendPath + "contatos.php", contato).success(function(data){
+
+    contato.serial = serialGenerator.generate();
+
+    contatosAPI.saveContato(contato).success(function(data){
       delete $scope.contato;
       $scope.contatoForm.$setPristine();
-      //carregarContatos();
+      carregarContatos();
     });
   };
   $scope.apagarContato = function(contatos){
@@ -43,4 +43,7 @@ app.controller("listaTelefonicaCtrl", function($scope, $http){
     $scope.criterioDeOrdenacao = campo
     $scope.direcaoDaOrdenacao = ! $scope.direcaoDaOrdenacao;
   };
+
+  carregarOperadoras();
+  carregarContatos();
 });
---
+++ rbranas/listaTelefonica/js/services/contatosAPIService.js
@@ -0,0 +1,16 @@
+app.factory("contatosAPI", function($http, config){
+
+  var _getContatos = function(){
+    return $http.get( config.baseUrl + "backend/contatos.json");
+  }
+
+  var _saveContato = function(contato){
+    return $http.post( config.baseUrl + "backend/contatos.php", contato);
+  }
+
+  return {
+    getContatos: _getContatos,
+    saveContato: _saveContato
+  }
+
+});
